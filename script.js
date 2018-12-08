886f7b23d5ccf92ed86fd95fe1d260b39b402f6f
Farah Quader <fyquader@gmail.com>
2018-12-07 23:32:31 -0800

    Hit spotify api

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const app = {};
 
+app.apiUrl = 'https://api.spotify.com/v1';
+
 app.events = function() {
     $('form').on('submit', function(e) {
         e.preventDefault();
         let artists = $('input[type=search]').val();
-        console.log(artists)
+        artists = artists.split(',');
+        let searched = artists.map(artistName => app.searchArtist(artistName));
+        console.log(searched);
     });
 };
 
-app.init() = function() {
+
+app.searchArtist = (artistName) => $.ajax({
+    url: '${api.apiUrl}/search',
+    method: 'GET',
+    dataType: 'jsonp',
+    data: {q: artistName}
+})
+app.init = function() {
     app.events();
 };
 
